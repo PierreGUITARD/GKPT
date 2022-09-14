a2ab72b4661366c5f4172573e79d6e4502d4128d
AzyLooper <kevinn246810@gmail.com>
2022-09-14 11:58:35 +0200

    ‎

---
+++ GKPT/includes/widget.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-1">
+        <h2>Small<br>Widget</h2>
+        <h3>Big description</h3>
+        <p>small description</p>
+        <h4>some detail</h4>
+        <button type="button" onclick='location.href="Assets/missing.png"'>local access</button>
+    </div>
+    <div class="col-2">
+        <img src="Assets/missing.png" class="thumbnail">
+    </div>
+</div>
--- GKPT/index.php
+++ GKPT/index.php
@@ -34,6 +34,54 @@ include(ROOT_PATH.'/includes/banner.php');
 <a href="html/hydraulis.html" title="machine à café"><img src="static/images/hydraulis.jpg" style="width: 500px; height: 500px"></img></a>
 <a href="html/bras_robot.html" title="machine à café"><img src="static/images/bras_robot.jpg" style="width: 500px; height: 500px"></img></a>
 <a href="html/bras_levier.html" title="machine à café"><img src="static/images/levier.jpg" style="width: 500px; height: 500px"></img></a>
+<div class="row">
+    <div class="col-1">
+        <h2>Machine à café</h2>
+        <h3>Big description</h3>
+        <p>small description</p>
+        <h4>some detail</h4>
+        <button type="button" onclick='location.href="html/machine_cafe.html"'>local access</button>
+    </div>
+    <div class="col-2">
+        <img src="static/images/machine_cafe.jpg" class="bigThumbnail">
+    </div>
+</div>
+<div class="row">
+    <div class="col-1">
+        <h2>Hydraulis</h2>
+        <h3>Big description</h3>
+        <p>small description</p>
+        <h4>some detail</h4>
+        <button type="button" onclick='location.href="html/hydraulis.html"'>local access</button>
+    </div>
+    <div class="col-2">
+        <img src="static/images/hydraulis.jpg" class="bigThumbnail">
+    </div>
+</div>
+<div class="row">
+    <div class="col-1">
+        <h2>Small<br>Widget</h2>
+        <h3>Big description</h3>
+        <p>small description</p>
+        <h4>some detail</h4>
+        <button type="button" onclick='location.href="Assets/missing.png"'>local access</button>
+    </div>
+    <div class="col-2">
+        <img src="Assets/missing.png" class="thumbnail">
+    </div>
+</div>
+<div class="row">
+    <div class="col-1">
+        <h2>Small<br>Widget</h2>
+        <h3>Big description</h3>
+        <p>small description</p>
+        <h4>some detail</h4>
+        <button type="button" onclick='location.href="Assets/missing.png"'>local access</button>
+    </div>
+    <div class="col-2">
+        <img src="Assets/missing.png" class="thumbnail">
+    </div>
+</div>
 <!-- footer -->
 
 <?php 

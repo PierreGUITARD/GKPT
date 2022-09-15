324fe9ed3a630354ff13ba09acbfba006f613d3a
Gaëtan <gaetan.styskal@gmail.com>
2022-09-15 16:11:29 +0200

    modif/test iframe machine_cafe + css et favicon

--- GKPT/includes/head_section.php
+++ GKPT/includes/head_section.php
@@ -4,5 +4,7 @@
 <!-- Polices Google -->
 <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
 <!-- Référence à la feuille de styles -->
-<link rel="stylesheet" href="static/css/public_styling.css"> 
+<link rel="stylesheet" href="static/css/public_styling.css"/> 
+<!-- Favicon -->
+<link rel="icon" type="image/png" href=""/>
 <meta charset="UTF-8"> 

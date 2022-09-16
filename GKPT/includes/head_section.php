0fa2a5612e62de972c282b3edb8dcace8b12b3e7
Gaëtan <98382841+Zigatar1@users.noreply.github.com>
2022-09-16 13:52:07 +0200

    ajout logo + modif iframe

--- GKPT/includes/head_section.php
+++ GKPT/includes/head_section.php
@@ -5,6 +5,6 @@
 <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
 <!-- Référence à la feuille de styles -->
 <link rel="stylesheet" href="static/css/public_styling.css"/> 
-<!-- Favicon -->
-<link rel="icon" type="image/png" href=""/>
+<!-- Logo -->
+<link rel="icon" type="image/png" href="static/images/favicons/conveyor.png" />
 <meta charset="UTF-8"> 

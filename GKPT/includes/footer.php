545ae62412c08a5062396ee120ea90566995d3ad
AzyLooper <kevinn246810@gmail.com>
2022-09-21 08:51:48 +0200

    gkpt

--- GKPT/includes/footer.php
+++ GKPT/includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
 
-<p align="center"> Pierre Guitard • Thomas Sengat Kuo • Gaëtan Styskal • Kevin Nunes Ribeiro &copy; <?php echo date('Y'); ?></p>
+<p align="center">Gaëtan Styskal • Kevin Nunes Ribeiro • Pierre Guitard • Thomas Sengat Kuo &copy; <?php echo date('Y'); ?></p>
 
 </footer> 

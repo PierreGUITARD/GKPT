d19413441a8044c68e87ff7f00efc12f72d4ec20
AzyLooper <kevinn246810@gmail.com>
2022-11-17 11:23:03 +0100

    Update footer.php

--- GKPT/includes/footer.php
+++ GKPT/includes/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-<p style="text-align: center;"><a href="https://github.com/PierreGUITARD/GKPT" style="text-shadow: 1px 1px 2px grey; text-decoration: underline;">Projet GKPT</a> • Pierre Guitard • Kevin Nunes Ribeiro • Thomas Sengat Kuo • Gaëtan Styskal &copy; <?php echo date('M Y'); ?></p>
+<p style="text-align: center;"><a href="https://github.com/PierreGUITARD/GKPT" style="text-shadow: 1px 1px 2px grey; text-decoration: underline;">Projet GKPT</a> • Pierre Guitard • Kevin Nunes Ribeiro • Thomas Sengat Kuo • Gaëtan Styskal • Sarah Faye • &copy; <?php echo date('M Y'); ?></p>
 </footer> 
